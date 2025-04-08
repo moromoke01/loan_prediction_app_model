b8f8404231bc00efbdaf40c056f0a8b1c1951136
moromoke01 <bellomoromoke2019@gmail.com>
2025-04-08 18:01:02 +0100

    run port added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -46,4 +47,6 @@ def predict():
     return render_template('index.html', prediction_text=f'Loan Status: {result}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
